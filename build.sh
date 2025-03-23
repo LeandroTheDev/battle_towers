ba7a250f61361be6f1c05d961aa5b445dab330bc
LeandroTheDev <leandro.schmidt.profissional@gmail.com>
2025-03-23 12:39:19 -0300

    1.0.1 (BTNoToolsPatch)

---
+++ build.sh
@@ -0,0 +1,11 @@
+rm -rf "$VINTAGE_STORY/Mods/battletowers"
+mkdir -p "$VINTAGE_STORY/Mods/battletowers"
+cp -r ./BattleTowers/* "$VINTAGE_STORY/Mods/battletowers"
+
+rm -rf "$VINTAGE_STORY/Mods/btnotoolspatch"
+mkdir -p "$VINTAGE_STORY/Mods/btnotoolspatch"
+cp -r ./BTNoToolsPatch/BTNoToolsPatch/* "$VINTAGE_STORY/Mods/btnotoolspatch"
+
+rm -rf "$VINTAGE_STORY/Mods/bttranslocatorengineeringpatch"
+mkdir -p "$VINTAGE_STORY/Mods/bttranslocatorengineeringpatch"
+cp -r ./BTTranslocatorEngineeringPatch/BTTranslocatorEngineeringPatch/* "$VINTAGE_STORY/Mods/bttranslocatorengineeringpatch"
