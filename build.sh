7084816e0d55bdd95f76338240eb56fdc69d1a3d
LeandroTheDev <leandro.schmidt.profissional@gmail.com>
2025-08-12 19:03:47 -0300

    1.0.7

--- build.sh
+++ build.sh
@@ -1,19 +1,29 @@
-rm -rf "$VINTAGE_STORY/Mods/battletowers"
-mkdir -p "$VINTAGE_STORY/Mods/battletowers"
-cp -r ./BattleTowers/* "$VINTAGE_STORY/Mods/battletowers"
+#!/bin/sh
+if [ -z "$VINTAGE_STORY_MODS" ] || [ ! -d "$VINTAGE_STORY_MODS" ]; then
+    echo "VINTAGE_STORY_MODS undefined or invalid directory."
+    read -p "Vintage Story mods path: " VINTAGE_STORY_MODS
 
-rm -rf "$VINTAGE_STORY/Mods/btnotoolspatch"
-mkdir -p "$VINTAGE_STORY/Mods/btnotoolspatch"
-cp -r ./BTNoToolsPatch/BTNoToolsPatch/* "$VINTAGE_STORY/Mods/btnotoolspatch"
+    if [ ! -d "$VINTAGE_STORY_MODS" ]; then
+        mkdir -p "$VINTAGE_STORY_MODS"
+    fi
+fi
 
-rm -rf "$VINTAGE_STORY/Mods/bttranslocatorengineeringpatch"
-mkdir -p "$VINTAGE_STORY/Mods/bttranslocatorengineeringpatch"
-cp -r ./BTTranslocatorEngineeringPatch/BTTranslocatorEngineeringPatch/* "$VINTAGE_STORY/Mods/bttranslocatorengineeringpatch"
+rm -rf "$VINTAGE_STORY_MODS/battletowers"
+mkdir -p "$VINTAGE_STORY_MODS/battletowers"
+cp -r ./BattleTowers/* "$VINTAGE_STORY_MODS/battletowers"
 
-rm -rf "$VINTAGE_STORY/Mods/btentities"
-mkdir -p "$VINTAGE_STORY/Mods/btentities"
-cp -r ./BTEntities/BTEntities/* "$VINTAGE_STORY/Mods/btentities"
+rm -rf "$VINTAGE_STORY_MODS/btnotoolspatch"
+mkdir -p "$VINTAGE_STORY_MODS/btnotoolspatch"
+cp -r ./BTNoToolsPatch/BTNoToolsPatch/* "$VINTAGE_STORY_MODS/btnotoolspatch"
 
-rm -rf "$VINTAGE_STORY/Mods/btrustboundpatch"
-mkdir -p "$VINTAGE_STORY/Mods/btrustboundpatch"
-cp -r ./BTRustboundPatch/BTRustboundPatch/* "$VINTAGE_STORY/Mods/btrustboundpatch"
+rm -rf "$VINTAGE_STORY_MODS/bttranslocatorengineeringpatch"
+mkdir -p "$VINTAGE_STORY_MODS/bttranslocatorengineeringpatch"
+cp -r ./BTTranslocatorEngineeringPatch/BTTranslocatorEngineeringPatch/* "$VINTAGE_STORY_MODS/bttranslocatorengineeringpatch"
+
+rm -rf "$VINTAGE_STORY_MODS/btentities"
+mkdir -p "$VINTAGE_STORY_MODS/btentities"
+cp -r ./BTEntities/BTEntities/* "$VINTAGE_STORY_MODS/btentities"
+
+rm -rf "$VINTAGE_STORY_MODS/btrustboundpatch"
+mkdir -p "$VINTAGE_STORY_MODS/btrustboundpatch"
+cp -r ./BTRustboundPatch/BTRustboundPatch/* "$VINTAGE_STORY_MODS/btrustboundpatch"
